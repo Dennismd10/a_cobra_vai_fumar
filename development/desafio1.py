0fe256ad2f1a1b3a81cec3292ef94b95074db55f
Dennis M. Dutra <dennismd10@gmail.com>
2025-09-24 21:58:47 -0300

    Adição: Arquivo inicial de código Python.

---
+++ development/desafio1.py
@@ -0,0 +1,12 @@
+"""
+-> Comentários de multiplas linhas. (Padrão estipulado pela turma de IA)
+"""
+
+'''
+-> Comentários de multiplas linhas.
+'''
+
+# Comentário usando uma única linha.
+
+# print() é usado para saída de dados.
+print("Hello World!")
