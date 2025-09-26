19c99a8cb76ff2843e57f7c87e07f242f5316eed
Dennis M. Dutra <dennismd10@gmail.com>
2025-09-25 21:45:38 -0300

    Avanço nos arquivos desafio2 e 3

---
+++ development/desafio2.py
@@ -0,0 +1,11 @@
+import os
+
+print("Python School")
+
+# Declaração de variável.
+nome_da_variavel = input("Digite algum tipo de dado...")
+print("nome_da_variavel")
+print(type(nome_da_variavel))
+
+os.system("pause")
+os.system("cls")
