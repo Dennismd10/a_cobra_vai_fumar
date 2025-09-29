d23991eadee399c35aa6bb063755b1a7c33cdd1b
Dennis M. Dutra <dennismd10@gmail.com>
2025-09-29 19:43:48 -0300

    Atualização da estrutura de arquivos desafio.py

---
+++ development/desafio_03.py
@@ -0,0 +1,10 @@
+"""
+
+  Crie um algoritmo que leia o nome da categoria e armazene em uma variável
+
+
+"""
+
+categoria = input("Digite a categoria do curso:")
+print(categoria)
+print(type(categoria))
---
+++ development/desafio_04.py
@@ -0,0 +1,12 @@
+"""
+
+   Somar dois números
+   Sem Debug
+
+
+
+"""
+x = int(input("digite um número"))
+y = int(input("digite outro número"))
+soma = x + y
+print(soma)
